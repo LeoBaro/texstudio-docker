55cbab59a762673f98353c73c19ee2c423f4c1ca
Leonardo Baroncelli <leonardo.baroncelli@inaf.it>
2022-09-30 17:06:56 +0200

    Add check on the existence of required env var

--- start_texstudio.sh
+++ start_texstudio.sh
@@ -3,4 +3,9 @@
 export UID=$(id -u)
 export GID=$(id -g)
 
+if [ -z "$MOUNT_DIR" ]; then
+    echo "MOUNT_DIR is not set. Please set it to the directory you want to mount."
+    exit 1
+fi
+
 docker-compose up -d texstudio
